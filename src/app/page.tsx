ade447ad771bb2777e9df392cc628d7d65f9342b
Junhyuck <mrbartrns@naver.com>
2024-07-25 15:01:24 +0900

    chore: install react-flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import dynamic from 'next/dynamic';
+
+const ReactFlowPage = dynamic(() => import('~/react-flow/react-flow-page'), {
+  ssr: false,
+});
+
 const Page = () => {
   return (
     <main>
       <h1 className="text-xl font-bold">React-flow Playground</h1>
+      <ReactFlowPage />
     </main>
   );
 };
---
+++ src/react-flow/react-flow-page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+const ReactFlowPage = () => {
+  return <div>Page</div>;
+};
+
+export default ReactFlowPage;
