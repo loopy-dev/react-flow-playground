bd9aa294902349520539af5dafdb1e4ddc9f6ce8
Junhyuck <mrbartrns@naver.com>
2024-07-25 14:50:36 +0900

    chore: set development settings

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 80,
+  endOfLine: "lf",
+  plugin: ["prettier-plugin-tailwindcss"],
+};
